169a646cb57fdd65a8cebaabb37046d4ecf36d58
Akira NISHIDA <nishida@ssisc.org>
2014-12-16 12:19:59 +0900

    bumped version number to 1.5.39

--- include/lis.h
+++ include/lis.h
@@ -27,7 +27,7 @@
 #ifndef __LIS_H__
 #define __LIS_H__
 /**************************************/
-#define LIS_VERSION	"1.5.38"
+#define LIS_VERSION	"1.5.39"
 /**************************************/
 #include <stdio.h>
 
