deacb7f1a3cce2c89f5c7412e3d0ebd95b3bde58
Akira NISHIDA <nishida@ssisc.org>
2015-01-10 11:43:33 +0900

    Bump version number to 1.5.47

--- include/lis.h
+++ include/lis.h
@@ -27,7 +27,7 @@
 #ifndef __LIS_H__
 #define __LIS_H__
 /**************************************/
-#define LIS_VERSION	"1.5.46"
+#define LIS_VERSION	"1.5.47"
 /**************************************/
 #include <stdio.h>
 
