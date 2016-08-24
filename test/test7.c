fc50b6e5d3d08c92ebf741f8bdf5c06961eccb30
Akira NISHIDA <nishida@ssisc.org>
2016-08-24 23:44:10 +0900

    Fix notation

--- test/test7.c
+++ test/test7.c
@@ -47,12 +47,12 @@ LIS_INT main(LIS_INT argc, char* argv[])
 
 #if defined(_Complex_I)	
 	z = 1.2 + 3.4 * _Complex_I;
+	printf("complex number z = %f + %f * I\n", creal(z), cimag(z));
 #else
 	z[0] = 1.2;
 	z[1] = 3.4;
+	printf("complex number z = %f + %f * I\n", z[0], z[1]);
 #endif
-
-	printf("complex number z = %f + %f * I\n", creal(z), cimag(z));
 
 	return 0;
 }
