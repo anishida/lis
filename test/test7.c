6ece1458cc07878a18dab3deffb0454bb8b16a44
Akira NISHIDA <nishida@ssisc.org>
2016-08-24 20:41:52 +0900

    Add sample programs for complex numbers

---
+++ test/test7.c
@@ -0,0 +1,65 @@
+/* Copyright (C) 2005 The Scalable Software Infrastructure Project. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+   1. Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+   3. Neither the name of the project nor the names of its contributors 
+      may be used to endorse or promote products derived from this software 
+      without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE SCALABLE SOFTWARE INFRASTRUCTURE PROJECT
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE SCALABLE SOFTWARE INFRASTRUCTURE
+   PROJECT BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+   OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+   INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+   ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+   POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifdef HAVE_CONFIG_H
+        #include "lis_config.h"
+#else
+#ifdef HAVE_CONFIG_WIN_H
+        #include "lis_config_win.h"
+#endif
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "lis.h"
+
+
+#undef __FUNC__
+#define __FUNC__ "main"
+LIS_INT main(LIS_INT argc, char* argv[])
+{
+	LIS_COMPLEX z;
+
+	/* define complex number */
+#if defined(_Complex_I)	
+	z = 1.2 + 3.4 * _Complex_I;
+#else
+	z.R = 1.2;
+	z.C = 3.4;
+#endif
+
+#if defined(_Complex_I)		
+	printf("complex number z = %f + %f * I\n", creal(z), cimag(z));
+#else
+	printf("complex number z = %f + %f * I\n", z.R, z.C);	
+#endif
+
+	return 0;
+}
+
+
