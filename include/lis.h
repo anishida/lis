098ae83b39c31a2ebb8b3c1a306aabdafaa92392
Akira NISHIDA <nishida@ssisc.org>
2016-10-25 03:32:25 +0900

    Bump version number to 1.7.2

--- include/lis.h
+++ include/lis.h
@@ -27,7 +27,7 @@
 #ifndef __LIS_H__
 #define __LIS_H__
 /**************************************/
-#define LIS_VERSION	"1.7.1"
+#define LIS_VERSION	"1.7.2"
 /**************************************/
 #include <stdio.h>
 #ifdef HAVE_COMPLEX_H
