27d03963db792063e6275f1f37b61dc923a1f175
Akira NISHIDA <nishida@ssisc.org>
2016-11-03 12:45:07 +0900

    Add support for lis_solve_setup

--- src/fortran/lisf_solver.c
+++ src/fortran/lisf_solver.c
@@ -250,6 +250,19 @@ void lis_solve_kernel_f(LIS_MATRIX_F *A, LIS_VECTOR_F b, LIS_VECTOR_F x, LIS_SOL
 }
 
 #undef __FUNC__
+#define __FUNC__ "lis_solve_setup_f"
+void lis_solve_setup_f(LIS_MATRIX_F *A, LIS_SOLVER_F *solver, LIS_INT *ierr)
+{
+	LIS_DEBUG_FUNC_IN;
+
+	*ierr = lis_solve_setup((LIS_MATRIX)LIS_V2P(A), (LIS_SOLVER)LIS_V2P(solver));
+	if( *ierr )	return;
+
+	LIS_DEBUG_FUNC_OUT;
+	return;
+}
+
+#undef __FUNC__
 #define __FUNC__ "lis_solver_get_precon_f"
 void lis_solver_get_precon_f(LIS_SOLVER_F *solver, LIS_INT *nsol, LIS_INT *ierr)
 {
