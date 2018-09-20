e848ac6845687ccf007766225dc277daf5ec294a
Akira NISHIDA <nishida@ssisc.org>
2018-09-20 21:46:43 +0900

    Bump version number to 2.0.15

--- include/lis.h
+++ include/lis.h
@@ -33,7 +33,7 @@ extern "C" {
 #endif
 
 /**************************************/
-#define LIS_VERSION	"2.0.14"
+#define LIS_VERSION	"2.0.15"
 /**************************************/
 #include <stdio.h>
 #ifdef USE_COMPLEX
