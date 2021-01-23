795ad07d6a40eb65652259f0b69b9a153dbbe653
Akira NISHIDA <nishida@ssisc.org>
2021-01-23 14:35:13 +0900

    Bump version number to 2.0.30

--- include/lis_config_win.h
+++ include/lis_config_win.h
@@ -16,11 +16,11 @@
 #define PACKAGE_NAME "lis"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "lis 2.0.29"
+#define PACKAGE_STRING "lis 2.0.30"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "lis"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.29"
+#define PACKAGE_VERSION "2.0.30"
 
