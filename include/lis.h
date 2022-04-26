0d4efddbf46bb95415aa75951abf2a319a94e7d8
Akira NISHIDA <nishida@ssisc.org>
2022-04-26 17:51:52 +0900

    Bump version number to 2.0.34

--- include/lis.h
+++ include/lis.h
@@ -33,7 +33,7 @@ extern "C" {
 #endif
 
 /**************************************/
-#define LIS_VERSION	"2.0.33"
+#define LIS_VERSION	"2.0.34"
 /**************************************/
 #include <stdio.h>
 #ifdef USE_COMPLEX
