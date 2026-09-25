156f4599d94cbd3b987b91c93d2332c3eaf0734d
Akira NISHIDA <nishida@ssisc.org>
2026-09-25 09:20:46 +0900

    Fix bugs in lis_matrix_g2l_csr(), where g2l_map was checked instead of l2g_map.

--- src/matrix/lis_matrix_mpi.c
+++ src/matrix/lis_matrix_mpi.c
@@ -160,7 +160,7 @@ LIS_INT lis_matrix_g2l_csr(LIS_MATRIX A)
 	}
 
 	l2g_map = (LIS_INT *)lis_malloc( (np-n)*sizeof(LIS_INT) );
-	if( g2l_map==NULL )
+	if( l2g_map==NULL )
 	{
 		lis_hashtable_destroy(g2l_map);
 		LIS_SETERR_MEM((np-n)*sizeof(LIS_INT));
@@ -263,7 +263,7 @@ LIS_INT lis_matrix_g2l_csr(LIS_MATRIX A)
 	}
 
 	l2g_map = (LIS_INT *)lis_malloc( (np-n)*sizeof(LIS_INT),"lis_matrix_g2l_csr::l2g_map" );
-	if( g2l_map==NULL )
+	if( l2g_map==NULL )
 	{
 		lis_free(g2l_map);
 		LIS_SETERR_MEM((np-n)*sizeof(LIS_INT));
